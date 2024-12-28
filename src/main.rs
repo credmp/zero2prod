f6a4777a01e226ffb7b17601bd80554ba12ddaa0
Arjen Wiersma <arjen@wiersma.org>
2024-12-28 23:18:16 +0100

    follow: 3.8.5.5

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::net::TcpListener;
 
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let listener = TcpListener::bind("127.0.0.1:8000").expect("failed to bind to port");
     run(listener)?.await
 }
