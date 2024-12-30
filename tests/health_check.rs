21227db7bd846dceed6891f10c986a30f9153c75
Arjen Wiersma <arjen@wiersma.org>
2024-12-30 15:42:16 +0100

    follow: Chapter 6 complete

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -157,7 +157,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
     // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
@@ -177,9 +177,9 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
             .expect("Failed to execute request.");
         // Assert
         assert_eq!(
-            200,
+            400,
             response.status().as_u16(),
-            "The API did not return a 200 OK when the payload was {}.",
+            "The API did not return a 400 OK when the payload was {}.",
             description
         );
     }
